ea0c516c61dd78af46ef43307b90c3ba656fc4f9
janjao937 <janjao937@gmail.com>
2024-09-11 14:36:39 +0700

    card input

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour,IPointerDownHandler
 {
+
    public void OnClickCard(){
-    
+        Debug.Log($"Click this card {this.gameObject.name}");
    }
+    public void OnPointerDown(PointerEventData eventData){
+      OnClickCard();
+    }
 }
