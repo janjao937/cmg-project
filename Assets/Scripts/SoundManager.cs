437db8a8437df8a59c1b887cfe2ab183f2bc628c
janjao937 <janjao937@gmail.com>
2024-09-12 11:56:02 +0700

    sound manager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,32 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AudioEnum{
+public enum Sound{
     BUTTON_CLICK,
     ADD_SCORE,
     ADD_TURN,
     GAME_END,
-    // BGM,
+    BGM,
+    FLIP_CARD,
 
 }
 [System.Serializable]
-public struct MatchAudioClips{
-    public AudioEnum AudioEnum;
+public struct SoundClips{
+    public Sound Sound;
     public AudioClip AudioClip;
+    [Range(0, 1)] public float SoundVolume;
 }
 
 public class SoundManager : MonoBehaviour
 {
-    // [SerializeField] private AudioSource bgmAudioSource;
+    public static SoundManager Instance {get;private set;}
     [SerializeField]private AudioSource sfxAudioSource;
-    [SerializeField] private MatchAudioClips[] matchAudioClips;
+    [SerializeField]private AudioSource bgmAudioSource;
+    [SerializeField] private SoundClips[] soundClips;
 
-    void Start()
+    private void Awake()
     {
-        
+        if(Instance==null){
+            Instance = this;
+        }
     }
-    void Update()
+
+    private SoundClips GetAudioClip(Sound sound)
     {
-        
+        foreach (var soundClip in soundClips)
+        {
+            if (soundClip.Sound == sound)
+            {
+                return soundClip;
+            }
+        }
+
+        return default(SoundClips);
     }
+    private void Play(AudioSource audioSource, Sound sound)
+    {
+
+        var soundClip = GetAudioClip(sound);
+        audioSource.clip = soundClip.AudioClip;
+        audioSource.volume = soundClip.SoundVolume;
+        audioSource.Play();
+       
+    }
+    public void PlaySFX(Sound sfx){
+         sfxAudioSource.loop = false;
+        Play(sfxAudioSource,sfx);
+    }
+    public void PlayBGM(Sound bgm)
+    {
+        bgmAudioSource.loop = true;
+        Play(bgmAudioSource, bgm);
+    }   
 }
